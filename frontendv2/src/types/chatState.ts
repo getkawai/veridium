b38924d576c1c7fcaa051464918ad21bcd9a61a3
aaprama <aprama@icloud.com>
2025-11-16 18:05:04 +0700

    remove frontendv2

--- frontendv2/src/types/chatState.ts
+++
@@ -1,8 +0,0 @@
-export enum ChatState {
-  Idle = 'idle',
-  Thinking = 'thinking',
-  Streaming = 'streaming',
-  WaitingForUserInput = 'waitingForUserInput',
-  Compacting = 'compacting',
-  LoadingConversation = 'loadingConversation',
-}
