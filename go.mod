7d8d7dd228afc963d1f69f03f5626c1568d9e029
aaprama <aprama@icloud.com>
2025-12-25 22:53:02 +0700

    feat(tunnelkit): implement tunnel management functions including creation, retrieval, and DNS routing

--- go.mod
+++ go.mod
@@ -11,6 +11,7 @@ require (
 	github.com/charmbracelet/x/exp/slice v0.0.0-20251201173703-9f73bfd934ff
 	github.com/charmbracelet/x/json v0.2.0
 	github.com/cloudflare/cloudflare-go v0.114.0
+	github.com/cloudflare/cloudflared v0.0.0-20251121053309-0d2a7a0385bb
 	github.com/dslipak/pdf v0.0.2
 	github.com/duckdb/duckdb-go/v2 v2.5.3
 	github.com/ebitengine/purego v0.9.1
@@ -23,10 +24,12 @@ require (
 	github.com/nikolalohinski/gonja/v2 v2.5.0
 	github.com/openai/openai-go/v2 v2.7.1
 	github.com/pemistahl/lingua-go v1.4.0
+	github.com/rs/zerolog v1.34.0
 	github.com/sahilm/fuzzy v0.1.1
 	github.com/ssor/bom v0.0.0-20170718123548-6386211fdfcf
 	github.com/stretchr/testify v1.11.1
 	github.com/tyler-smith/go-bip39 v1.1.0
+	github.com/urfave/cli/v2 v2.27.5
 	github.com/wailsapp/wails/v3 v3.0.0-dev
 	github.com/yuin/goldmark v1.7.8
 	golang.org/x/net v0.47.0
@@ -40,6 +43,7 @@ require (
 require (
 	dario.cat/mergo v1.0.1 // indirect
 	git.sr.ht/~jackmordaunt/go-toast/v2 v2.0.3 // indirect
+	github.com/BurntSushi/toml v1.4.0 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
 	github.com/ProjectZKM/Ziren/crates/go-runtime/zkvm_runtime v0.0.0-20251001021608-1fe7b43fc4d6 // indirect
 	github.com/ProtonMail/go-crypto v1.1.6 // indirect
@@ -48,6 +52,8 @@ require (
 	github.com/VictoriaMetrics/fastcache v1.13.0 // indirect
 	github.com/adrg/xdg v0.5.3 // indirect
 	github.com/apache/arrow-go/v18 v18.4.1 // indirect
+	github.com/apparentlymart/go-cidr v1.1.0 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bep/debounce v1.2.1 // indirect
 	github.com/bits-and-blooms/bitset v1.20.0 // indirect
 	github.com/blevesearch/go-porterstemmer v1.0.3 // indirect
@@ -65,7 +71,12 @@ require (
 	github.com/cloudflare/circl v1.6.0 // indirect
 	github.com/cloudwego/base64x v0.1.6 // indirect
 	github.com/consensys/gnark-crypto v0.18.0 // indirect
+	github.com/coredns/caddy v1.1.2-0.20241029205200-8de985351a98 // indirect
+	github.com/coredns/coredns v1.12.2 // indirect
+	github.com/coreos/go-oidc/v3 v3.10.0 // indirect
+	github.com/coreos/go-systemd/v22 v22.5.0 // indirect
 	github.com/couchbase/vellum v1.0.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.5 // indirect
 	github.com/crate-crypto/go-eth-kzg v1.4.0 // indirect
 	github.com/crate-crypto/go-ipa v0.0.0-20240724233137-53bbb0ceb27a // indirect
 	github.com/cyphar/filepath-securejoin v0.4.1 // indirect
@@ -86,19 +97,30 @@ require (
 	github.com/ethereum/c-kzg-4844/v2 v2.1.5 // indirect
 	github.com/ethereum/go-bigmodexpfix v0.0.0-20250911101455-f9e208c548ab // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
+	github.com/facebookgo/grace v0.0.0-20180706040059-75cf19382434 // indirect
 	github.com/ferranbt/fastssz v0.1.4 // indirect
+	github.com/flynn/go-shlex v0.0.0-20150515145356-3f9db97f8568 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
 	github.com/gabriel-vasile/mimetype v1.4.9 // indirect
+	github.com/getsentry/sentry-go v0.27.0 // indirect
 	github.com/gin-contrib/sse v1.1.0 // indirect
 	github.com/glycerine/go-unsnap-stream v0.0.0-20181221182339-f9677308dec2 // indirect
+	github.com/go-chi/chi/v5 v5.2.2 // indirect
+	github.com/go-chi/cors v1.2.1 // indirect
 	github.com/go-git/gcfg v1.5.1-0.20230307220236-3a3c6141e376 // indirect
 	github.com/go-git/go-billy/v5 v5.6.2 // indirect
 	github.com/go-git/go-git/v5 v5.13.2 // indirect
+	github.com/go-jose/go-jose/v4 v4.1.1 // indirect
 	github.com/go-json-experiment/json v0.0.0-20251027170946-4849db3c2f7e // indirect
+	github.com/go-logr/logr v1.4.3 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/go-playground/validator/v10 v10.27.0 // indirect
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/gobwas/ws v1.2.1 // indirect
 	github.com/goccy/go-json v0.10.5 // indirect
 	github.com/goccy/go-yaml v1.19.0 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
@@ -108,7 +130,10 @@ require (
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/flatbuffers v25.2.10+incompatible // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
-	github.com/gorilla/websocket v1.4.2 // indirect
+	github.com/google/gopacket v1.1.19 // indirect
+	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/grpc-ecosystem/grpc-gateway/v2 v2.26.3 // indirect
+	github.com/grpc-ecosystem/grpc-opentracing v0.0.0-20180507213350-8e809c8a8645 // indirect
 	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
 	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/huin/goupnp v1.3.0 // indirect
@@ -129,15 +154,20 @@ require (
 	github.com/mattn/go-colorable v0.1.14 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/mattn/go-runewidth v0.0.16 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.4 // indirect
+	github.com/miekg/dns v1.1.66 // indirect
 	github.com/minio/sha256-simd v1.0.0 // indirect
-	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/mitchellh/go-homedir v1.1.0 // indirect
+	github.com/mitchellh/mapstructure v1.5.1-0.20231216201459-8508981c8b6c // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/mschoch/smat v0.2.0 // indirect
+	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.2.4 // indirect
-	github.com/philhofer/fwd v1.0.0 // indirect
+	github.com/philhofer/fwd v1.1.3-0.20240916144458-20a13a1f6b7c // indirect
 	github.com/pierrec/lz4/v4 v4.1.22 // indirect
 	github.com/pion/dtls/v2 v2.2.7 // indirect
 	github.com/pion/logging v0.2.2 // indirect
@@ -148,11 +178,15 @@ require (
 	github.com/pkg/browser v0.0.0-20240102092130-5ac0b6a4141c // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
+	github.com/prometheus/client_golang v1.22.0 // indirect
+	github.com/prometheus/client_model v0.6.2 // indirect
+	github.com/prometheus/common v0.64.0 // indirect
+	github.com/prometheus/procfs v0.15.1 // indirect
 	github.com/quic-go/qpack v0.5.1 // indirect
 	github.com/quic-go/quic-go v0.54.0 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
 	github.com/rivo/uniseg v0.4.7 // indirect
-	github.com/rogpeppe/go-internal v1.13.1 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
 	github.com/samber/lo v1.49.1 // indirect
 	github.com/sergi/go-diff v1.4.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
@@ -166,34 +200,51 @@ require (
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
 	github.com/tidwall/sjson v1.2.5 // indirect
-	github.com/tinylib/msgp v1.1.0 // indirect
-	github.com/tklauser/go-sysconf v0.3.12 // indirect
-	github.com/tklauser/numcpus v0.6.1 // indirect
+	github.com/tinylib/msgp v1.2.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.14 // indirect
+	github.com/tklauser/numcpus v0.8.0 // indirect
 	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
 	github.com/ugorji/go/codec v1.3.0 // indirect
 	github.com/wailsapp/go-webview2 v1.0.22 // indirect
 	github.com/wailsapp/mimetype v1.4.1 // indirect
 	github.com/willf/bitset v1.1.10 // indirect
 	github.com/xanzy/ssh-agent v0.3.3 // indirect
+	github.com/xrash/smetrics v0.0.0-20240521201337-686a1a2994c1 // indirect
 	github.com/zeebo/xxh3 v1.0.2 // indirect
 	go.etcd.io/bbolt v1.3.5 // indirect
-	go.uber.org/mock v0.5.0 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/propagators v0.22.0 // indirect
+	go.opentelemetry.io/otel v1.37.0 // indirect
+	go.opentelemetry.io/otel/exporters/otlp/otlptrace v1.26.0 // indirect
+	go.opentelemetry.io/otel/metric v1.37.0 // indirect
+	go.opentelemetry.io/otel/sdk v1.37.0 // indirect
+	go.opentelemetry.io/otel/trace v1.37.0 // indirect
+	go.opentelemetry.io/proto/otlp v1.2.0 // indirect
+	go.uber.org/automaxprocs v1.6.0 // indirect
+	go.uber.org/mock v0.5.1 // indirect
 	golang.org/x/arch v0.20.0 // indirect
 	golang.org/x/crypto v0.44.0 // indirect
 	golang.org/x/exp v0.0.0-20250606033433-dcc06ee1d476 // indirect
 	golang.org/x/mod v0.29.0 // indirect
+	golang.org/x/oauth2 v0.30.0 // indirect
 	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/telemetry v0.0.0-20251008203120-078029d740a8 // indirect
 	golang.org/x/tools v0.38.0 // indirect
 	golang.org/x/xerrors v0.0.0-20240903120638-7835f813f4da // indirect
+	google.golang.org/genproto/googleapis/api v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250707201910-8d1bb00bc6a7 // indirect
+	google.golang.org/grpc v1.75.0 // indirect
 	google.golang.org/protobuf v1.36.10 // indirect
 	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.2.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	modernc.org/libc v1.61.13 // indirect
 	modernc.org/mathutil v1.7.1 // indirect
 	modernc.org/memory v1.8.2 // indirect
+	nhooyr.io/websocket v1.8.7 // indirect
+	zombiezen.com/go/capnproto2 v2.18.0+incompatible // indirect
 )
 
 replace github.com/wailsapp/wails/v3 => ./wails/v3
---
+++ pkg/tunnelkit/tunnelkit.go
@@ -0,0 +1,485 @@
+package tunnelkit
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"time"
+
+	"github.com/cloudflare/cloudflared/cfapi"
+	"github.com/cloudflare/cloudflared/cmd/cloudflared/cliutil"
+	"github.com/cloudflare/cloudflared/cmd/cloudflared/tunnel"
+	"github.com/cloudflare/cloudflared/connection"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog"
+	"github.com/urfave/cli/v2"
+)
+
+// Config holds the configuration for creating and running a tunnel
+type Config struct {
+	AccountID  string
+	APIToken   string
+	TunnelName string
+	Hostname   string
+	LocalURL   string
+}
+
+// TunnelInfo contains information about a created tunnel
+type TunnelInfo struct {
+	TunnelID    string
+	TunnelToken string
+	Hostname    string
+	PublicURL   string
+}
+
+// GetOrCreateTunnelWithDNS gets an existing tunnel by name or creates a new one if it doesn't exist
+func GetOrCreateTunnelWithDNS(cfg Config) (*TunnelInfo, error) {
+	// Get zone ID from hostname
+	zoneID, err := getZoneID(cfg.APIToken, cfg.Hostname)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get zone ID: %w", err)
+	}
+
+	// Create REST client
+	logger := zerolog.Nop()
+	client, err := cfapi.NewRESTClient(
+		"https://api.cloudflare.com/client/v4",
+		cfg.AccountID,
+		zoneID,
+		cfg.APIToken,
+		"tunnelkit",
+		&logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	// Try to find existing tunnel by name
+	filter := cfapi.NewTunnelFilter()
+	filter.ByName(cfg.TunnelName)
+	filter.NoDeleted()
+
+	tunnels, err := client.ListTunnels(filter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list tunnels: %w", err)
+	}
+
+	var tunnelID uuid.UUID
+	var tunnelToken string
+
+	if len(tunnels) > 0 {
+		// Use existing tunnel
+		tunnel := tunnels[0]
+		tunnelID = tunnel.ID
+
+		// Check if tunnel has active connections
+		activeClients, err := client.ListActiveClients(tunnelID)
+		if err == nil && len(activeClients) > 0 {
+			return nil, fmt.Errorf("tunnel '%s' is currently in use with %d active connection(s)", cfg.TunnelName, len(activeClients))
+		}
+
+		// Get tunnel token
+		tunnelToken, err = client.GetTunnelToken(tunnelID)
+		if err != nil {
+			return nil, fmt.Errorf("failed to get tunnel token: %w", err)
+		}
+	} else {
+		// Create new tunnel
+		tunnelSecret, err := generateTunnelSecret()
+		if err != nil {
+			return nil, fmt.Errorf("failed to generate tunnel secret: %w", err)
+		}
+
+		tunnelResult, err := client.CreateTunnel(cfg.TunnelName, tunnelSecret)
+		if err != nil {
+			return nil, fmt.Errorf("failed to create tunnel: %w", err)
+		}
+
+		tunnelID = tunnelResult.Tunnel.ID
+		tunnelToken = tunnelResult.Token
+	}
+
+	// Create or update DNS route
+	route := cfapi.NewDNSRoute(cfg.Hostname, true)
+	_, err = client.RouteTunnel(tunnelID, route)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create DNS route: %w", err)
+	}
+
+	// Update tunnel configuration with ingress rules
+	err = updateTunnelConfig(cfg.APIToken, cfg.AccountID, tunnelID, cfg.Hostname, cfg.LocalURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update tunnel config: %w", err)
+	}
+
+	info := &TunnelInfo{
+		TunnelID:    tunnelID.String(),
+		TunnelToken: tunnelToken,
+		Hostname:    cfg.Hostname,
+		PublicURL:   fmt.Sprintf("https://%s", cfg.Hostname),
+	}
+
+	return info, nil
+}
+
+// CreateTunnelWithDNS creates a new Cloudflare Tunnel and sets up DNS routing
+func CreateTunnelWithDNS(cfg Config) (*TunnelInfo, error) {
+	// Generate tunnel secret
+	tunnelSecret, err := generateTunnelSecret()
+	if err != nil {
+		return nil, fmt.Errorf("failed to generate tunnel secret: %w", err)
+	}
+
+	// Get zone ID from hostname
+	zoneID, err := getZoneID(cfg.APIToken, cfg.Hostname)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get zone ID: %w", err)
+	}
+
+	// Create REST client
+	logger := zerolog.Nop()
+	client, err := cfapi.NewRESTClient(
+		"https://api.cloudflare.com/client/v4",
+		cfg.AccountID,
+		zoneID,
+		cfg.APIToken,
+		"tunnelkit",
+		&logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	// Create tunnel
+	tunnelResult, err := client.CreateTunnel(cfg.TunnelName, tunnelSecret)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create tunnel: %w", err)
+	}
+
+	// Create DNS route
+	route := cfapi.NewDNSRoute(cfg.Hostname, true)
+	_, err = client.RouteTunnel(tunnelResult.Tunnel.ID, route)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create DNS route: %w", err)
+	}
+
+	// Update tunnel configuration with ingress rules
+	err = updateTunnelConfig(cfg.APIToken, cfg.AccountID, tunnelResult.Tunnel.ID, cfg.Hostname, cfg.LocalURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to update tunnel config: %w", err)
+	}
+
+	info := &TunnelInfo{
+		TunnelID:    tunnelResult.Tunnel.ID.String(),
+		TunnelToken: tunnelResult.Token,
+		Hostname:    cfg.Hostname,
+		PublicURL:   fmt.Sprintf("https://%s", cfg.Hostname),
+	}
+
+	return info, nil
+}
+
+// GetTunnelByName retrieves an existing tunnel by name
+func GetTunnelByName(accountID, apiToken, tunnelName string) (*TunnelInfo, error) {
+	// Create REST client (zoneID not needed for listing tunnels)
+	logger := zerolog.Nop()
+	client, err := cfapi.NewRESTClient(
+		"https://api.cloudflare.com/client/v4",
+		accountID,
+		"", // zoneID not required for tunnel operations
+		apiToken,
+		"tunnelkit",
+		&logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	// Find tunnel by name
+	filter := cfapi.NewTunnelFilter()
+	filter.ByName(tunnelName)
+	filter.NoDeleted()
+
+	tunnels, err := client.ListTunnels(filter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list tunnels: %w", err)
+	}
+
+	if len(tunnels) == 0 {
+		return nil, fmt.Errorf("tunnel '%s' not found", tunnelName)
+	}
+
+	tunnel := tunnels[0]
+
+	// Get tunnel token
+	tunnelToken, err := client.GetTunnelToken(tunnel.ID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to get tunnel token: %w", err)
+	}
+
+	// Check if tunnel has active connections
+	activeClients, err := client.ListActiveClients(tunnel.ID)
+	isActive := err == nil && len(activeClients) > 0
+
+	info := &TunnelInfo{
+		TunnelID:    tunnel.ID.String(),
+		TunnelToken: tunnelToken,
+		Hostname:    "", // Will be populated from routes if needed
+		PublicURL:   "", // Will be populated from routes if needed
+	}
+
+	// Add active status info
+	if isActive {
+		return nil, fmt.Errorf("tunnel '%s' is currently active with %d connection(s)", tunnelName, len(activeClients))
+	}
+
+	return info, nil
+}
+
+// ListTunnels lists all tunnels in the account
+func ListTunnels(accountID, apiToken string) ([]*TunnelInfo, error) {
+	// Create REST client
+	logger := zerolog.Nop()
+	client, err := cfapi.NewRESTClient(
+		"https://api.cloudflare.com/client/v4",
+		accountID,
+		"",
+		apiToken,
+		"tunnelkit",
+		&logger,
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create API client: %w", err)
+	}
+
+	// List all non-deleted tunnels
+	filter := cfapi.NewTunnelFilter()
+	filter.NoDeleted()
+
+	tunnels, err := client.ListTunnels(filter)
+	if err != nil {
+		return nil, fmt.Errorf("failed to list tunnels: %w", err)
+	}
+
+	var result []*TunnelInfo
+	for _, tunnel := range tunnels {
+		// Check if tunnel has active connections
+		activeClients, _ := client.ListActiveClients(tunnel.ID)
+		isActive := len(activeClients) > 0
+
+		info := &TunnelInfo{
+			TunnelID:  tunnel.ID.String(),
+			Hostname:  "", // Not available from list API
+			PublicURL: "",
+		}
+
+		// Add status indicator in hostname field for display
+		if isActive {
+			info.Hostname = fmt.Sprintf("%s (ACTIVE - %d connections)", tunnel.Name, len(activeClients))
+		} else {
+			info.Hostname = fmt.Sprintf("%s (inactive)", tunnel.Name)
+		}
+
+		result = append(result, info)
+	}
+
+	return result, nil
+}
+
+// RunTunnel runs a Cloudflare Tunnel using the provided token
+func RunTunnel(ctx context.Context, tunnelToken string) error {
+	// Decode tunnel token
+	tokenBytes, err := base64.StdEncoding.DecodeString(tunnelToken)
+	if err != nil {
+		return fmt.Errorf("failed to decode tunnel token: %w", err)
+	}
+
+	var token connection.TunnelToken
+	if err := json.Unmarshal(tokenBytes, &token); err != nil {
+		return fmt.Errorf("failed to parse tunnel token: %w", err)
+	}
+
+	credentials := token.Credentials()
+	tunnelProps := &connection.TunnelProperties{
+		Credentials: credentials,
+	}
+
+	// Setup logger
+	logger := zerolog.New(zerolog.ConsoleWriter{Out: os.Stdout, TimeFormat: time.RFC3339}).
+		With().
+		Timestamp().
+		Logger()
+
+	// Create build info
+	buildInfo := &cliutil.BuildInfo{
+		GoOS:               "darwin",
+		GoVersion:          "go1.21",
+		CloudflaredVersion: "dev",
+	}
+
+	// Create flag set with required flags
+	set := flag.NewFlagSet("tunnel", flag.ContinueOnError)
+	set.String("protocol", "http2", "Protocol to use")
+	set.String("edge-ip-version", "auto", "Edge IP version")
+	set.Duration("rpc-timeout", 5*time.Second, "RPC timeout")
+	set.Int("retries", 5, "Number of retries")
+	set.Bool("no-tls-verify", true, "Disable TLS verification")
+	set.String("origin-ca-pool", "", "Origin CA pool")
+	set.String("config", "", "Config file path")
+
+	// Parse empty args
+	if err := set.Parse([]string{}); err != nil {
+		return fmt.Errorf("failed to parse flags: %w", err)
+	}
+
+	// Create cli context
+	cliCtx := cli.NewContext(nil, set, nil)
+
+	// Start tunnel server
+	return tunnel.StartServer(cliCtx, buildInfo, tunnelProps, &logger)
+}
+
+// RunTunnelWithShutdown runs a tunnel with graceful shutdown handling
+func RunTunnelWithShutdown(tunnelToken string) error {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Setup signal handling
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+
+	errChan := make(chan error, 1)
+
+	// Run tunnel in goroutine
+	go func() {
+		errChan <- RunTunnel(ctx, tunnelToken)
+	}()
+
+	// Wait for signal or error
+	select {
+	case sig := <-sigChan:
+		fmt.Printf("\nReceived signal: %v, shutting down...\n", sig)
+		cancel()
+		time.Sleep(2 * time.Second)
+		return nil
+	case err := <-errChan:
+		return err
+	}
+}
+
+// Helper functions
+
+func generateTunnelSecret() ([]byte, error) {
+	secret := make([]byte, 32)
+	id := uuid.New()
+	copy(secret, id[:])
+	return secret, nil
+}
+
+func getZoneID(apiToken, hostname string) (string, error) {
+	domain := extractDomain(hostname)
+
+	// Use HTTP client directly for zone lookup
+	client := &http.Client{Timeout: 15 * time.Second}
+
+	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones?name=%s", domain), nil)
+	if err != nil {
+		return "", err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	type ZoneResponse struct {
+		Result []struct {
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"result"`
+		Success bool `json:"success"`
+	}
+
+	var zoneResp ZoneResponse
+	if err := json.NewDecoder(resp.Body).Decode(&zoneResp); err != nil {
+		return "", err
+	}
+
+	if !zoneResp.Success || len(zoneResp.Result) == 0 {
+		return "", fmt.Errorf("zone not found for domain: %s", domain)
+	}
+
+	return zoneResp.Result[0].ID, nil
+}
+
+func extractDomain(hostname string) string {
+	parts := strings.Split(hostname, ".")
+	if len(parts) >= 2 {
+		return strings.Join(parts[len(parts)-2:], ".")
+	}
+	return hostname
+}
+
+func updateTunnelConfig(apiToken, accountID string, tunnelID uuid.UUID, hostname, localURL string) error {
+	type IngressRule struct {
+		Hostname string `json:"hostname,omitempty"`
+		Service  string `json:"service"`
+	}
+
+	type TunnelConfig struct {
+		Ingress []IngressRule `json:"ingress"`
+	}
+
+	config := TunnelConfig{
+		Ingress: []IngressRule{
+			{
+				Hostname: hostname,
+				Service:  localURL,
+			},
+			{
+				Service: "http_status:404",
+			},
+		},
+	}
+
+	configBytes, err := json.Marshal(config)
+	if err != nil {
+		return err
+	}
+
+	// Use HTTP client directly
+	client := &http.Client{Timeout: 15 * time.Second}
+	url := fmt.Sprintf("https://api.cloudflare.com/client/v4/accounts/%s/cfd_tunnel/%s/configurations", accountID, tunnelID)
+
+	req, err := http.NewRequest("PUT", url, bytes.NewBuffer(configBytes))
+	if err != nil {
+		return err
+	}
+
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", apiToken))
+	req.Header.Set("Content-Type", "application/json")
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		return fmt.Errorf("failed to update config: status %d", resp.StatusCode)
+	}
+
+	return nil
+}
