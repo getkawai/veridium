b7a49d95eb110b08cfc4cf225e0935671f1c4795
aaprama <aprama@icloud.com>
2026-01-09 00:24:40 +0800

    chore: update Sentry DSN in main.tsx
    Replaced the commented-out Sentry DSN with a new one for improved error tracking. This change ensures that the application is correctly configured to report errors to the updated Sentry project.

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import * as Sentry from "@sentry/react";
 
 // "https://5fb55a3957c99692d702a33e2993cc55@o4510639245426688.ingest.us.sentry.io/4510639291498496"
+// "https://f73dd13f253093e990baf69b9c652b76@o4510675714703360.ingest.us.sentry.io/4510675718832128"
 Sentry.init({
   dsn: "https://b66f862d7567c075a44c697757bb8130@o4510618985758720.ingest.us.sentry.io/4510618990804992",
   sendDefaultPii: true,
